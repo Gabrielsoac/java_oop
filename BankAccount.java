0b21fa589201608efa0219b552daeda8aacecc38
Gabriel Soares <gabrielsoacc@gmail.com>
2023-09-04 09:39:25 -0300

    adjusted fix error Scanner Close

--- BankAccount.java
+++ BankAccount.java
@@ -1,6 +1,9 @@
 package learning_oop.bank;
 
+import java.util.Scanner;
+
 public class BankAccount {
+    Scanner input = new Scanner(System.in);
     private int numberBank_account;
     private String nameHolder_account;
     private double balance;
@@ -28,14 +31,29 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
-    public void depositMoney(double valueMoney){
+    public void depositMoney(double depositValue){
+        this.balance += depositValue;
+        System.out.printf("New balance update as: %.2f\n", getBalance());
+    }
+    public void depositMoney(){
+        double valueMoney = input.nextDouble();
         this.balance += valueMoney;
         System.out.printf("New balance update as: %.2f\n", getBalance());
     }
-    public void withdrawMoney(double valueMoney){
+    public void withdrawMoney(){
+        double valueMoney = input.nextDouble();
         this.balance = balance - withDrawTax - valueMoney;
+
+        System.out.println("Tax value:" + withDrawTax);
+        System.out.printf("New balance update as: %.2f\n", getBalance());
+    }
+    public void withdrawMoney(double withDrawValue){
+
+        this.balance = balance - withDrawTax - withDrawValue;
+
         System.out.println("Tax value:" + withDrawTax);
         System.out.printf("New balance update as: %.2f", getBalance());
+
     }
     public String toString(){
         return String.format("Name: %s\n", getNameHolderAccount())
